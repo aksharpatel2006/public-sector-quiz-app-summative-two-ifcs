d47927eb642c039300a19edb81a355c448eb4d77
aksharpatel2006 <patel.a54@nulondon.ac.uk>
2026-06-09 15:03:20 +0100

    Updated app.py to include info screen

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Main Entry Point for Application
 
 import streamlit as st
 from screens.welcome_screen import WelcomeScreen
+from screens.info_screen import InfoScreen
 
 st.set_page_config(
      page_title="Public Sector Quiz",
@@ -15,3 +16,9 @@ if "screen" not in st.session_state:
 
 if st.session_state.screen == "welcome":
     WelcomeScreen().render()
+
+elif st.session_state.screen == "info":
+    InfoScreen().render()
+
+elif st.session_state.screen == "quiz":
+    st.title("Quiz Screen Coming soon..")
